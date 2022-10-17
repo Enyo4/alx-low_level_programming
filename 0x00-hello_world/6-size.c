13b7dc0065da8b654ce284974c34c70bf3085406
Eniola <morakinyoelisha01@gmail.com>
2022-10-17 10:30:26 -0700

    number 6 commit

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main- print out sizes
+ *
+ * Return: 0
+ */
+int main(void)
+{
+  int a;
+  long int b;
+  long long int c;
+  float d;
+  printf("Size of a char: %lu  byte(s)\n", sizeof(charType));
+  printf("Size of a int: %lu  byte(s)\n", sizeof(intType));
+  printf("Size of a char: %lu  byte(s)\n", sizeof(longint
+Size of an int: 4 byte(s)
+Size of a long int: 4 byte(s)
+Size of a long long int: 8 byte(s)
+	 Size of a float: 4 byte(s)
