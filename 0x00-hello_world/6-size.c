64bd8455835fd6a25e30fe3b9ad93fca82fbd6f9
Eniola <morakinyoelisha01@gmail.com>
2022-10-17 14:20:32 -0700

    finnaly done with 6-errors

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,21 +1,21 @@
-Up#include <stdio.h>
+#include <stdio.h>
 
 /**
  * main- print out sizes
  *
  * Return: 0
  */
-main(void)
+int main(void)
 {
-  int a;
-  long int b;
-  long long int c;
-  float d;
-  char e;
-        printf("Size of a char: %zu byte(s)\n", sizeof(e)); 
-         printf("Size of an int: %zu byte(s)\n", sizeof(a)); 
-         printf("Size of a long int: %zu byte(s)\n", sizeof(b)); 
-         printf("Size of a long long int: %zu byte(s)\n", sizeof(long long c)); 
-         printf("Size of a float: %zu byte(s)\n", sizeof(d)); 
-         return (0); 
- }
+int a;
+long int b;
+long long int c;
+float d;
+char e;
+printf("Size of char: %zu bytes\n", sizeof(e));
+printf("Size of int: %zu bytes\n", sizeof(a));
+printf("Size of long int: %zu bytes\n", sizeof(b));
+printf("Size of long long int: %zu bytes\n", sizeof(c));
+printf("Size of float: %zu bytes\n", sizeof(d));
+return (0);
+}
