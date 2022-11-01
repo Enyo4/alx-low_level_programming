a32490778b174d49d714cf9e4a6d6185fe15b31a
Eniola <morakinyoelisha01@gmail.com>
2022-11-01 06:39:30 -0700

    0 problem

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -7,6 +7,7 @@
  * @n - number of times to copy b
  * Return: pointer to the memory area (s)
  */
+
 char *_memset(char *s, char b, unsigned int n)
 {
 unsigned int i;
@@ -14,6 +15,7 @@ unsigned int i;
 for (i = 0; i < n; i++)
 {
 s[i] = b;
+}
 
 return (s);
 }
