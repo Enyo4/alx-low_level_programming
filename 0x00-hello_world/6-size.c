3e57523f3b11d70bc5ad07b5e87170cea381280c
Enyo4 <113777537+Enyo4@users.noreply.github.com>
2022-12-01 22:29:44 +0100

    Update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,10 +12,10 @@ long int b;
 long long int c;
 float d;
 char e;
-printf("Size of char: %zu byte(s)\n", sizeof(e));
-printf("Size of int: %zu byte(s)\n", sizeof(a));
-printf("Size of long int: %zu byte(s)\n", sizeof(b));
-printf("Size of long long int: %zu byte(s)\n", sizeof(c));
-printf("Size of float: %zu byte(s)\n", sizeof(d));
+printf("Size of a char: %zu byte(s)\n", sizeof(e));
+printf("Size of an int: %zu byte(s)\n", sizeof(a));
+printf("Size of a long int: %zu byte(s)\n", sizeof(b));
+printf("Size of a long long int: %zu byte(s)\n", sizeof(c));
+printf("Size of a float: %zu byte(s)\n", sizeof(d));
 return (0);
 }
