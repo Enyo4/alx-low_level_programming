f1b3b633e62e7e1582a1f413c43084ee08e68bb1
Eniola <morakinyoelisha01@gmail.com>
2022-12-31 02:44:03 -0800

    task 6 correct

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -7,13 +7,11 @@
 
 int main(void)
 {
-        int n;
+	for (n = 0; n < 10; n++)
+	{
+		putchar((n % 10) + '0');
+	}
+	putchar('\n');
 
-        for (n = '0'; n <= '9'; n++)
-        {
-                putchar(n);
-        }
-        putchar('\n');
-
-        return (0);
+	return (0);
 }
