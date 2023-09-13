e0818040e0e09470341b1ffa9ddff9dbca3f9d8d
Eniola <morakinyoelisha01@gmail.com>
2023-09-13 15:51:43 -0700

    Task 11 added

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_to_98 - Prints all natural mumbers from n to 98
+ * @n: number to be computed
+ *
+ * Return: 0 on success
+ */
+void print_to_98(int n)
+{
+	for (n = n; n <= 98; n++)
+	{
+		if (n == 98)
+		{
+			printf("%d\n", n);
+		}
+		else
+		{
+			printf("%d, ", n);
+		}
+	}
+}
