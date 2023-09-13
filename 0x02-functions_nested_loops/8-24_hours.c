9e8a801480ab5a3c07e342955120c0a689a57ef3
Eniola <morakinyoelisha01@gmail.com>
2023-09-13 15:05:37 -0700

    Task 8 added

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * jack_bauer - Prints evert minute of the day
+ * starting from 00:00 t0 23:59
+ *
+ * Return: 0 0n success
+ */
+void jack_bauer(void)
+{
+	int f, s, t, l;
+
+	for (f = 0; f < 3; f++)
+	{
+		for (s = 0; s < 4; s++)
+		{
+			for (t = 0; t <= 5; t++)
+			{
+				for (l = 0; l <= 9; l++)
+				{
+					_putchar(f + '0');
+					_putchar(s + '0');
+					_putchar(58);
+					_putchar(t + '0');
+					_putchar(l + '0');
+					_putchar('\n');
+				}
+			}
+		}
+	}
+}
