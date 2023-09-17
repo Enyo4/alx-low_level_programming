15687d2199126f309f78d081ac76c0d9d1f4028e
Eniola <morakinyoelisha01@gmail.com>
2023-09-17 14:13:26 -0700

    Task 6 added

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_line - Draws a straight line
+ * @n: number of times the character _ should be printed
+ *
+ * Return: 0 
+ */
+void print_line(int n)
+{
+	int a;
+
+	for (a = 0; a <= n; a++)
+	{
+		if (a == 0 || a < 0)
+		{
+			continue;
+		}
+		_putchar(95);
+	}
+	_putchar('\n');
+}
