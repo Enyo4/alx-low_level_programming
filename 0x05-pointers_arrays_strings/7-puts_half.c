7ed609a302e7d0a07b1c9a85be68b4cc74778ddf
Eniola <morakinyoelisha01@gmail.com>
2023-09-19 06:57:45 -0700

    Task 7 added

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * puts_half - Prints half of the string
+ * @str: Given string
+ *
+ * Return: half of the string
+ */
+void puts_half(char *str)
+{
+	int i, j, len = 0;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		len++;
+	}
+	if (len % 2 == 0)
+	{
+		j = len / 2;
+		for (; j < len; j++)
+		{
+			_putchar(str[j]);
+		}
+	}
+	else
+	{
+		j = (len - 1) / 2;
+		for (; j < len; j++)
+		{
+			_putchar(str[j]);
+		}
+	}
+	_putchar('\n');
+}
