076b6422627d3615ec5151a7c2c66356a9078d23
Eniola <morakinyoelisha01@gmail.com>
2023-09-20 11:22:36 -0700

    Task 0 fix

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,27 +9,19 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	int i = 0;
-	int j = 0;
-	int k, l, m, a;
+	char *s = dest;
 
-	while (*src != '\0')
-	{
-		i++;
-		src++;
-	}
 	while (*dest != '\0')
 	{
-		j++;
 		dest++;
 	}
-	k = i - 1;
-	l = j - 1;
-	m = k + l;
-	for (l = j - 1, a = 0; l < m && a < k; l++, a++)
+	while (*src != '\0')
 	{
-		dest[l] = src[a];
+		*dest = *src;
+		dest++;
+		src++;
 	}
-	dest[l] = '\0';
-	return (dest);
+	*dest = '\0';
+	return (s);
+
 }
