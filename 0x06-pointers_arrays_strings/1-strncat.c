5400510a65ebc8e3aca8c27ebe88c76484c9afdd
Eniola <morakinyoelisha01@gmail.com>
2023-09-20 11:45:08 -0700

    fix

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -4,6 +4,7 @@
  * _strncat - Concatenates two strings with at most n bytes from src.
  * @src: source string that is concatenated
  * @dest: string where @src is added to
+ * @n: The lengyh of the int
  *
  * Return: pointer to dest
  */
@@ -18,8 +19,8 @@ char *_strncat(char *dest, char *src, int n)
 
 	for (a = 0; src[a] != '\0' && a < n; a++)
 	{
-		dest[d] = src[a];
+		dest[d + a] = src[a];
 	}
-	dest[d] = '\0';
+	dest[d + a] = '\0';
 	return (dest);
 }
