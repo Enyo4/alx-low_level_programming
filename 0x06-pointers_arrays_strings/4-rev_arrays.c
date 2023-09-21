c7f4237a09ec7ccb3d6022f469f85a9af71792b2
Eniola <morakinyoelisha01@gmail.com>
2023-09-21 03:54:29 -0700

    added task 4

--- 0x06-pointers_arrays_strings/4-rev_arrays.c
+++ 0x06-pointers_arrays_strings/4-rev_arrays.c
@@ -12,10 +12,10 @@ void reverse_array(int *a, int n)
 	int i, j = n / 2;
 	int temp;
 
-	for (i = 0; i < j / 2; i++)
+	for (i = 0; i < j; i++)
 	{
 		temp = a[i];
-		a[i] = a[j - i];
-		a[j - i] = temp;
+		a[i] = a[n - 1 - i];
+		a[n - 1 - i] = temp;
 	}
 }
