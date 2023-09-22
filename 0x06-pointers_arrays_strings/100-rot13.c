e976cf8b18a3847fe323e6866dead5acca738086
Eniola <morakinyoelisha01@gmail.com>
2023-09-21 18:25:34 -0700

    Rot13 encoder

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * rot13 - encodes a string by moving it 13 places in the alphabetical order
+ * @str: Given string
+ *
+ * Return: The encoded string
+ */
+char *rot13(char *str)
+{
+	int i, j;
+	char a[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char b[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (j = 0; a[j] != '\0'; j++)
+		{
+			if ( str[i] == a[j])
+			{
+				str[i] = b[j];
+				break;
+			}
+		}
+	}
+	return (str);
+}
