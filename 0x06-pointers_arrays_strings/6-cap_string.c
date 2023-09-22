88ace88e3bd2bcf3fed97fc64a6b6aaa8c72ef12
Eniola <morakinyoelisha01@gmail.com>
2023-09-21 17:16:52 -0700

    Task 6 added

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * cap_string - Capitalizes all words of a string after a space or after
+ * Separators of words: space, tabulation,
+ * new line, ,, ;, ., !, ?, ", (, ), {, and }
+ * @str: The input string
+ *
+ * Return: The formatted string
+ */
+char *cap_string(char *str)
+{
+	int i, j;
+	char sc[] = {' ', '\t', '\n', 44, ';', 46, '!', '?', '"', '(', ')', '{', '}'};
+
+	for (i = 0; sc[i] != '\0'; i++)
+	{
+		for (j = 0; str[j] != '\0'; j++)
+		{
+			if ((str[j - 1] == sc[i] && str[j] >= 97 && str[j] <= 122))
+			{
+				str[j] -= 32;
+			}
+		}
+	}
+	return (str);
+}
