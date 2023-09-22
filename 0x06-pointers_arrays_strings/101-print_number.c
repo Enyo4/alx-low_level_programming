4c8629f911cdea574afff4762e88b5a2bcc5dcd8
Eniola <morakinyoelisha01@gmail.com>
2023-09-21 18:34:45 -0700

    Task 9 added

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_number - Prints an integer
+ * @n: Integer to print
+ *
+ * Return: void
+ */
+void print_number(int n)
+{
+	unsigned int i;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		n *= -1;
+	}
+	i = n;
+	if (i / 10)
+	{
+		print_number(i / 10);
+	}
+	_putchar(x % 10 + '0');
+}
