77e0fae1da58eb4ac97c97fbed9d3aab16060f82
Eniola <morakinyoelisha01@gmail.com>
2023-09-25 15:44:45 -0700

    Checker fix

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -9,18 +9,18 @@
  */
 char *_strchr(char *s, char c)
 {
-	int i;
+        int i;
 
-	for (i = 0; s[i] != '\0'; i++)
-	{
-		if (s[i] == c)
-		{
-			return (s + i);
-		}
-	}
-	if (c == '\0')
-	{
-		return (s);
-	}
-	return ('\0');
+        for (i = 0; s[i] != '\0'; i++)
+        {
+                if (s[i] == c)
+                {
+                        return (s + i);
+                }
+        }
+        if (c == '\0')
+        {
+                return (s);
+        }
+        return (NULL);
 }
