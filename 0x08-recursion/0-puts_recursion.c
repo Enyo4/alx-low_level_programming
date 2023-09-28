62d5f9bcabb6034f74855046c963b6e4de064874
Eniola <morakinyoelisha01@gmail.com>
2023-09-28 16:21:59 -0700

    Betty fix

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _puts_recursion: Prints a string, followed by anew line
+ * _puts_recursion - Prints a string, followed by anew line
  * @s: Input string
  *
  * Return: Nothing
