3f0bedf1f14f8d03e6af96bd1aaaf45049f776e1
Eniola <morakinyoelisha01@gmail.com>
2023-09-29 16:02:13 -0700

    Test fix

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,6 +1,33 @@
 #include "main.h"
 
 /**
+ * is_prime_checker - checks if the integer is prime
+ * @n: the number to check
+ * @divisor: the number to divide @n by
+ *
+ * Return: 1 if prime else 0
+ */
+int is_prime_checker(int n, int divisor)
+{
+	if (n <= 1)
+	{
+		return (0);
+	}
+	else if (divisor == 1)
+	{
+		return (1);
+	}
+	else if (n % divisor == 0)
+	{
+		return (0);
+	}
+	else
+	{
+		return (is_prime_checker(n, divisor - 1));
+	}
+}
+
+/**
  * is_prime_number - Checks if input integer is prime number
  * @n: Input integer
  *
@@ -8,18 +35,5 @@
  */
 int is_prime_number(int n)
 {
-        int i;
-
-  if (n <= 1) {
-    return 0;
-  }
-
-  for (i = 2; i * i <= n; i++) {
-    if (n % i == 0) {
-      return 0;
-    }
-  }
-
-  return 1;
+	return (is_prime_checker(n, n / 2));
 }
-
