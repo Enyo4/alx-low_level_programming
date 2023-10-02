713404438bb2d784cffc2c29d94e2972fb0de95b
Eniola <morakinyoelisha01@gmail.com>
2023-10-02 16:00:12 -0700

    first commit

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Prints its name followed by a new line
+ *
+ * Return: 0
+ */
+int main(int __attribute__((unused)) argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
