df59ecfdca9b2702936f788cc7766df898b42b2a
Eniola <morakinyoelisha01@gmail.com>
2023-10-04 14:50:30 -0700

    test fix

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -32,9 +32,11 @@ int **alloc_grid(int width, int height)
 			{
 				free(grid[j]);
 			}
-			free(grid);
 			return (NULL);
 		}
+	}
+	for (i = 0; i < height; i++)
+	{
 		for (j = 0; j < width; j++)
 		{
 			grid[i][j] = 0;
