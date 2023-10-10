7b3260192534753a34699d52a475eb467be42035
Eniola <morakinyoelisha01@gmail.com>
2023-10-10 15:57:18 -0700

    My first commit

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - A dogs data
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: Pet's owner
+ *
+ * Description: Longer description
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
