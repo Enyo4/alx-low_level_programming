f8a1a91f867b5e8ae45a7106c0010a43ffeb5b3a
Eniola <morakinyoelisha01@gmail.com>
2023-10-12 10:26:05 -0700

    New fix

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -14,11 +14,6 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (size <= 0)
-	{
-		return (-1);
-	}
-
 	if (size > 0 && array != NULL && cmp != NULL)
 	{
 		for (i = 0; i < size; i++)
@@ -29,4 +24,6 @@ int int_index(int *array, int size, int (*cmp)(int))
 			}
 		}
 	}
+
+	return (-1);
 }
