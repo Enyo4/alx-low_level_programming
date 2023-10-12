7e7baae47e8e731f650c6391cc052397b41e4cfa
Eniola <morakinyoelisha01@gmail.com>
2023-10-12 15:59:03 -0700

    Task 3 added

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,41 @@
+#include "3-calc.h"
+
+/**
+ * main - performs simple operations.
+ * @argc: Number of arguments
+ * @argv: Array of integers
+ *
+ * Return: results
+ */
+int main(int argc, char *argv[])
+{
+	unsigned int num1, num2;
+	int (*oprt)(int, int);
+	char sign;
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+
+	oprt = get_op_func(argv[2]);
+	sign = *argv[2]; 
+
+	if ((sign == '/' || sign == '%') && num2 == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	if (!oprt)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	printf("%d\n", oprt(num1, num2));
+	return (0);
+}
