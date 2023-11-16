16cd4fa687e97edc6bbb575130c7dd8014edca2d
Eniola <morakinyoelisha01@gmail.com>
2023-11-15 16:16:34 -0800

    Task 2 fix

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,30 +1,23 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * binary_to_uint - convert a binary number to an unsigned int
- * @b: char string
- * Return: converted decimal number or 0 if there is an unconvertable char
+ * get_bit - Returns the value of a bit at a given index
+ * @n: The given character
+ * @index: number to print
+ * Return: The value of the bit at index index or -1 if an error occured
  */
-unsigned int binary_to_uint(const char *b)
+int get_bit(unsigned long int n, unsigned int index)
 {
-	unsigned int total, power;
-	int len;
-
-	if (b == NULL)
-		return (0);
-
-	for (len = 0; b[len]; len++)
+	int bit_value = -1;
+	
+	if (index < sizeof(unsigned long int) * 8)
 	{
-		if (b[len] != '0' && b[len] != '1')
-			return (0);
+		while (index > 0)
+		{
+			n >>= 1;
+			index--;
+		}
+		bit_value = n & 1;
 	}
-
-	for (power = 1, total = 0, len--; len >= 0; len--, power *= 2)
-	{
-		if (b[len] == '1')
-			total += power;
-	}
-
-	return (total);
+	return (bit_value);
 }
---
+++ 0x14-bit_manipulation/test.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+void print_binary(unsigned long int n)
+{
+	unsigned int i;
+
+	if (n == '0')
+	{
+		_putchar('0');
+	}
+
