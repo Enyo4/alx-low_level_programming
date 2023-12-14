28b52d70f211ac50963c17df0480f06bb738b1c7
Enyo4 <morakinyoelisha01@gmail.com>
2023-12-14 13:13:13 -0800

    new library

--- 0x18-dynamic_libraries/win.c
+++ 0x18-dynamic_libraries/win.c
@@ -4,5 +4,20 @@
 
 int rand(void)
 {
-	return (9);
+	static int ct = -1;
+
+	ct++;
+	if (ct == 0)
+		return 8;
+	if (ct == 1)
+		return 8;
+	if (ct == 2)
+		return 7;
+	if (ct == 3)
+		return 9;
+	if (ct == 4)
+		return 23;
+	if (ct == 5)
+		return 74;
+	return ct * ct % 30000;
 }
