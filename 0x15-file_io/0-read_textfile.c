b9f50cac8506df101d426e5eb8727923b5c412d4
Enyo4 <morakinyoelisha01@gmail.com>
2024-02-13 00:22:09 -0800

    Time to test

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,52 +1,54 @@
-#include "main.h"
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>/* For POSIX functions */
+#include <unistd.h>
+#include "main.h"
 
 /**
- * read_textfile - Reads a test file and prints it to the POSIX standard output
- * @filename: The name of the file
- * @letters: The number of letters to read and print
+ * read_textfile - Reads a text file and prints it to the POSIX standard output.
+ * @filename: File to be read
+ * @letters: Number of letters to read
  *
- * Return: The number of chars printed  if file name NULL
+ * Return: Actual number of letters it could read and print, or
+ *		0 if file can not be opened or read or NULL.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	ssize_t bytes_written, bytes_read;
-	char *buffer;
-	FILE *file;
+	int file;
+	char *buf;
+	ssize_t bytes_read, bytes_written;
 
 	if (filename == NULL)
+		return (0);
+
+	file = open(filename, O_RDONLY);
+	if (file == -1)
+		return (0);
+
+	buf = (char *)malloc(sizeof(char) * (letters + 1));
+	if (buf == NULL)
 	{
+		close(file);
 		return (0);
 	}
 
-	file = fopen(filename, "r");
-	if (file == NULL)
+	bytes_read = read(file, buf, letters);
+	if (bytes_read == -1)
 	{
+		close(file);
+		free(buf);
 		return (0);
 	}
 
-	buffer = (char *)malloc(letters);
-	if (buffer == NULL)
+	bytes_written = write(STDOUT_FILENO, buf, bytes_read);
+	if (bytes_written == -1 || bytes_written != bytes_read)
 	{
-		fclose(file);
+		close(file);
+		free(buf);
 		return (0);
 	}
-	bytes_read = fread(buffer, 1, letters, file);
-	if (bytes_read <= 0)
-	{
-		fclose(file);
-		free(buffer);
-		return (0);
-	}
-	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
 
-	fclose(file);
-	free(buffer);
-
-	if (bytes_written != bytes_read)
-	{
-		return (0);
-	}
+	close(file);
+	free(buf);
 	return (bytes_written);
 }
